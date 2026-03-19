c01e09ef27108ebb21b9e91511e8b198ca9dcbc3
ragu-kumar <kumarputhiren@gmail.com>
2026-03-19 20:00:51 +0530

    Add files via upload

--- src/main/java/utils/ExtentManager.java
+++ src/main/java/utils/ExtentManager.java
@@ -1,6 +1,7 @@
 package utils;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentManager {
 
@@ -8,7 +9,9 @@ public class ExtentManager {
 
     public static ExtentReports getInstance() {
         if (extent == null) {
+            ExtentSparkReporter reporter = new ExtentSparkReporter("reports/extent-report.html");
             extent = new ExtentReports();
+            extent.attachReporter(reporter);
         }
         return extent;
     }
